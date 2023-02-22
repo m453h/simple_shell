749aa4d0b0619f0cd1abc92d2f7b48ebbef76622
Michael Hudson <mnkotagu@gmail.com>
2023-02-22 22:24:20 +0300

    Implement shell main function

--- main.c
+++ main.c
@@ -1,35 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "shell.h"
 
 /**
-* main - simple shell main function
-*
-* Description: using the main function
-* this program executes shell commands
-* entered by the user.
-*
-* Return: (0)
-*/
-
-int main(void)
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-	char *buffer;
-	size_t buff_size = 32;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	buffer = malloc(buff_size * sizeof(char));
-
-	if (buffer == NULL)
+	if (ac == 2)
 	{
-		perror("Unable to allocate buffer");
-		exit(1);
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-
-	printf("$ ");
-	getline(&buffer, &buff_size, stdin);
-	printf("%s", buffer);
-
-
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
